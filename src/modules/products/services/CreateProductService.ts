fb1201bd8d4ec7236fc1c0ba90a0332f007ef117
Marcus Phellypp <marcus_psc@hotmail.com>
2025-01-28 13:00:08 -0300

    ♻️ Update CreateProductService to use ProductRepository

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -1,6 +1,6 @@
 // import { AppDataSource } from '../../../../data-source';
 import AppError from '@shared/errors/AppError';
-import productRepository from '../typeorm/repositories/ProductsRepository';
+import ProductRepository from '../typeorm/repositories/ProductsRepository';
 import Product from '../typeorm/entities/Product';
 
 interface IRequest {
@@ -11,19 +11,19 @@ interface IRequest {
 
 class CreateProductService {
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    const productExists = await productRepository.findByName(name);
+    const productExists = await ProductRepository.findByName(name);
 
     if (productExists) {
       throw new AppError('There is already one product with this name');
     }
 
-    const product = await productRepository.createProduct({
+    const product = await ProductRepository.createProduct({
       name,
       price,
       quantity,
     });
 
-    await productRepository.save(product);
+    await ProductRepository.save(product);
 
     return product;
   }
