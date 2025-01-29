47f304f9cf7ece432d98935781dd8d837abf5d9b
Marcus Phellypp <marcus_psc@hotmail.com>
2025-01-28 23:02:10 -0300

    ✨ Add ValidationError support to AppError class

--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -1,10 +1,14 @@
+import { ValidationError } from 'express-validator';
+
 class AppError {
   public readonly message: string;
   public readonly statusCode: number;
+  public readonly errors?: ValidationError[];
 
-  constructor(message: string, statusCode = 400) {
+  constructor(message: string, statusCode = 400, errors?: ValidationError[]) {
     this.message = message;
     this.statusCode = statusCode;
+    this.errors = errors;
   }
 }
 
