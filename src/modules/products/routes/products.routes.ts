1c57c3499333dce4d82f73508e38ec102057f478
Marcus Phellypp <marcus_psc@hotmail.com>
2025-01-28 23:03:39 -0300

    🔧 Update product routes to use validations and asyncHandler

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,17 +1,67 @@
-import { Router } from 'express';
+import { NextFunction, Router, Request, Response } from 'express';
+import { body, param } from 'express-validator';
 import ProductsController from '../controllers/ProductsController';
 import asyncHandler from 'express-async-handler';
 
 const productRouter = Router();
 const productsController = new ProductsController();
 
+//middlewares de validação
+const validateProductId = [
+  param('id').isUUID().withMessage('Id inválido'),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.params = req.params || {};
+    next();
+  },
+];
+
+const validateProductCreation = [
+  body('name').isString().notEmpty().withMessage('Nome é obrigatório'),
+  body('prive')
+    .isFloat({ gt: 0 })
+    .withMessage('Preço é obrigatório e deve ser maior que zero'),
+  body('quantity')
+    .isInt({ gt: 0 })
+    .withMessage('Quantidade é obrigatório e deve ser maior que zero'),
+];
+
 productRouter.get(
   '/',
   asyncHandler((req, res) => productsController.index(req, res)),
 );
-productRouter.get('/:id', (req, res) => productsController.show(req, res));
-productRouter.post('/', (req, res) => productsController.create(req, res));
-productRouter.put('/:id', (req, res) => productsController.update(req, res));
-productRouter.delete('/:id', (req, res) => productsController.delete(req, res));
+productRouter.get(
+  '/:id',
+  validateProductId,
+  asyncHandler((req: Request<{ id: string }>, res) =>
+    productsController.show(req, res),
+  ),
+);
+
+productRouter.post(
+  '/',
+  validateProductCreation,
+  asyncHandler((req, res) => productsController.create(req, res)),
+);
+
+productRouter.put(
+  '/:id',
+  [...validateProductId, ...validateProductCreation],
+  asyncHandler(
+    (
+      req: Request<
+        { id: string },
+        Record<string, unknown>,
+        { name: string; price: number; quantity: number }
+      >,
+      res,
+    ) => productsController.update(req, res),
+  ),
+);
+
+productRouter.delete(
+  '/:id',
+  validateProductId,
+  asyncHandler((req, res) => productsController.delete(req, res)),
+);
 
 export default productRouter;
